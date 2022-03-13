eef0e5c19257934c74975083822a1aab27a9bb9e
Sam Myers <github@sammye.rs>
2022-03-13 15:11:07 -0500

    Refactor to remove config crate (#9)
    * Refactor to remove config crate
    
    * Enable GitHub CI

--- src/spotify/cache.rs
+++ src/spotify/cache.rs
@@ -3,7 +3,7 @@ pub struct CacheKey {}
 pub const CACHE_SHOW_FOR_SECONDS: usize = 1200;
 
 impl CacheKey {
-    pub fn show_from_id(show_id: &String) -> String {
+    pub fn show_from_id(show_id: &str) -> String {
         format!("spotify:id:{}", show_id)
     }
 }
