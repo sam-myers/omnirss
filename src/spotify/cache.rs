4f6a9ca73af0115ec5eabe76c10bf1574d4f5c67
Sam Myers <github@sammye.rs>
2022-03-26 15:15:45 -0500

    Make cache keys lowercase

--- src/spotify/cache.rs
+++ src/spotify/cache.rs
@@ -5,10 +5,10 @@ pub const CACHE_SEARCH_FOR_SECONDS: usize = 600;
 
 impl CacheKey {
     pub fn show(show_id: &str) -> String {
-        format!("spotify:id:{}", show_id)
+        format!("spotify:id:{}", show_id.to_lowercase())
     }
 
     pub fn search(query: &str) -> String {
-        format!("spotify:search:{}", query)
+        format!("spotify:search:{}", query.to_lowercase())
     }
 }
