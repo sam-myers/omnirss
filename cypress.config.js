947e913f03307a83dc99157238ea3af3f06b4a7e
Sam Myers <sam.myers@protonmail.com>
2022-09-25 23:15:22 -0500

    Disable support file

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: "http://localhost:8888",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
