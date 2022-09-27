1b54a811634a01f3a30c1aac69f0342d2972186d
Sam Myers <sam.myers@protonmail.com>
2022-09-27 00:27:26 -0500

    Refactor out icon library

--- omnirss/src/App.svelte
+++ omnirss/src/App.svelte
@@ -1,3 +1,10 @@
+<script>
+  import Fa from 'svelte-fa'
+  import { faGithub } from '@fortawesome/free-brands-svg-icons';
+  import { faLock } from '@fortawesome/free-solid-svg-icons';
+  import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+</script>
+
 <main>
   <nav class="navbar navbar-light navbar-expand bg-light navigation-clean">
     <div class="container"><a class="navbar-brand" href="/">OmniRSS</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"></button>
@@ -31,21 +38,21 @@
       <div class="row">
         <div class="col-lg-4">
           <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
-            <div class="d-flex features-icons-icon"><i class="icon-puzzle m-auto text-primary" data-bss-hover-animate="pulse"></i></div>
+            <div class="icon"><Fa icon={faPuzzlePiece} rotate="-45" translateY="{-0.1}" /></div>
             <h3>Bring Your Own Client</h3>
             <p class="lead mb-0">Works with any podcatcher / RSS client.</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
-            <div class="d-flex features-icons-icon"><i class="icon-lock m-auto text-primary" data-bss-hover-animate="pulse"></i></div>
+            <div class="icon"><Fa icon={faLock} translateX="{0.03}"/></div>
             <h3>Private</h3>
             <p class="lead mb-0">No invasive trackers. No ads.</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
-            <div class="d-flex features-icons-icon"><i class="icon-social-github m-auto text-primary" data-bss-hover-animate="pulse"></i></div>
+            <div class="icon"><Fa icon={faGithub} /></div>
             <h3>Open Source</h3>
             <p class="lead mb-0"><a href="https://github.com/sam-myers/omnirss/">Source Code.</a> Permissively licensed.</p>
           </div>
@@ -81,3 +88,10 @@
     </div>
   </footer>
 </main>
+
+<style>
+  .icon {
+    font-size: 4.5rem;
+    color: rgb(0, 123, 255);
+  }
+</style>
